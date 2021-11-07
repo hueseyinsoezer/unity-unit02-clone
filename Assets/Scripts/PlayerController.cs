3d3f192fdbab3e7c4955c3fc99ad619300740957
hueseyinsoezer <huseyinsozer97@hotmail.com>
2021-11-07 22:56:49 +0300

    +Limited the range of motion for x coordinates

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -6,6 +6,7 @@ public class PlayerController : MonoBehaviour
 {
     public float horizontalInput;
     public float speed = 10.0f;
+    public float xRange = 20f;//Boundary of movements value
     
     // Start is called before the first frame update
     void Start()
@@ -18,5 +19,13 @@ public class PlayerController : MonoBehaviour
     {
         horizontalInput = Input.GetAxis("Horizontal");
         transform.Translate(Vector3.right*horizontalInput*Time.deltaTime*speed);
+        if (transform.position.x < -xRange)
+        {
+            transform.position = new Vector3(-xRange, transform.position.y,transform.position.z);
+        }
+        if (transform.position.x > xRange)
+        {
+            transform.position = new Vector3(xRange, transform.position.y, transform.position.z);
+        }
     }
 }
