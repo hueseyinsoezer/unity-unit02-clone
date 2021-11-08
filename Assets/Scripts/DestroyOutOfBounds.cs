a6460636dcf77554686113565a62a82dada095a4
hueseyinsoezer <huseyinsozer97@hotmail.com>
2021-11-08 15:03:40 +0300

    +Created a new script as SpawnManager

--- Assets/Scripts/DestroyOutOfBounds.cs
+++ Assets/Scripts/DestroyOutOfBounds.cs
@@ -4,11 +4,16 @@ using UnityEngine;
 
 public class DestroyOutOfBounds : MonoBehaviour
 {
-    private float topBound = 30.0f;
+    private float topBound = 20.0f;
+    private float lowerBound = -8.0f;
     void Update()
     {
 
         if (transform.position.z > topBound)
+        {
+            Destroy(gameObject);
+        }
+        else if (transform.position.z < lowerBound)
         {
             Destroy(gameObject);
         }
