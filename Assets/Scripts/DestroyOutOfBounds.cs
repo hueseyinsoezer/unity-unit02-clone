fef479e1462a2b4451f2b6b428ddcf5058f13b18
hueseyinsoezer <huseyinsozer97@hotmail.com>
2021-11-08 21:20:55 +0300

    +Added a game over comment for animals that can pass the player

--- Assets/Scripts/DestroyOutOfBounds.cs
+++ Assets/Scripts/DestroyOutOfBounds.cs
@@ -16,6 +16,7 @@ public class DestroyOutOfBounds : MonoBehaviour
         else if (transform.position.z < lowerBound)
         {
             Destroy(gameObject);
+            Debug.Log("Game Over!");
         }
 
     }
