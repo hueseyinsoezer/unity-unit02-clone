a73f4c6da11116015caeda4f10eeba23520975f0
hueseyinsoezer <huseyinsozer97@hotmail.com>
2021-11-08 13:19:23 +0300

    +Updated the movement speed of food prefabs

--- Assets/Scripts/MoveForward.cs
+++ Assets/Scripts/MoveForward.cs
@@ -4,7 +4,7 @@ using UnityEngine;
 
 public class MoveForward : MonoBehaviour
 {
-    public float speed;
+    public float speed = 20.0f;
     // Start is called before the first frame update
     void Start()
     {
