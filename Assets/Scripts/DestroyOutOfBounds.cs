676876b1a53c87b1156f81467dc4a52b12720a37
hueseyinsoezer <huseyinsozer97@hotmail.com>
2021-11-08 13:55:06 +0300

    +Created a new script for our food prefab. +Added a destroy function to remove food prefab when it run away from scene

---
+++ Assets/Scripts/DestroyOutOfBounds.cs
@@ -0,0 +1,17 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DestroyOutOfBounds : MonoBehaviour
+{
+    private float topBound = 30.0f;
+    void Update()
+    {
+
+        if (transform.position.z > topBound)
+        {
+            Destroy(gameObject);
+        }
+
+    }
+}
