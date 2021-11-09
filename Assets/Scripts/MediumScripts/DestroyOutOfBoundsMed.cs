dfe67ef5c8472a255f02cba7a00a3edbce8a305f
hueseyinsoezer <huseyinsozer97@hotmail.com>
2021-11-09 23:54:20 +0300

    + Added boundary for horizontal moving animals

--- Assets/Scripts/MediumScripts/DestroyOutOfBoundsMed.cs
+++ Assets/Scripts/MediumScripts/DestroyOutOfBoundsMed.cs
@@ -3,21 +3,39 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class DestroyOutOfBoundsMed : MonoBehaviour
-{
-    private float topBound = 20.0f;
-    private float lowerBound = -8.0f;
+{ 
+    private float topBoundZ = 20.0f;
+    private float lowerBoundZ = -8.0f;
+    private float sideBound = 25.0f;
+
     void Update()
-    {
+     {
 
-        if (transform.position.z > topBound)
-        {
-            Destroy(gameObject);
-        }
-        else if (transform.position.z < lowerBound)
-        {
-            Destroy(gameObject);
-            Debug.Log("Game Over!");
-        }
+         if (transform.position.z > topBoundZ)
+         {
+             Destroy(gameObject);
+         }
+         else if (transform.position.z < lowerBoundZ)
+         {
+             Destroy(gameObject);
+             Debug.Log("Game Over!");
+         }
+         else if (transform.position.x > sideBound)
+         {
+             Destroy(gameObject);
+             Debug.Log("Game Over!");
+         }
+         else if (transform.position.x < -sideBound)
+         {
+             Destroy(gameObject);
+             Debug.Log("Game Over!");
+         }
+       
 
-    }
+
+
+
+
+} 
+
 }
